84d3af41cfb3e244effac6453e5585860854304c
Mehak Chopra <m.chopra1@universityofgalway.ie>
2024-04-29 13:18:53 +0100

    Update PRscs.sh

--- PRscs.sh
+++ PRscs.sh
@@ -8,4 +8,4 @@
 #SBATCH --partition="normal","highmem"
 #SBATCH -a 1-22
 
-python PRScs.py --ref_dir=/data3/mchopra/PRScs-1.1.0/ref_ukbb_eur/ldblk_ukbb_eur --bim_prefix=/data4/mchopra/GWAS_PGS_training/training_data/c_1_22 --sst_file=/data4/mchopra/GWAS_PGS_training/training_data/fastgwapgs/aao_input_prscs_se.txt --n_gwas=33372 --chrom=${SLURM_ARRAY_TASK_ID} --out_dir=/data4/mchopra/GWAS_PGS_training/training_data/PRScs_results/aao_${SLURM_ARRAY_TASK_ID}
+python PRScs.py --ref_dir=/data3/mchopra/PRScs-1.1.0/ref_ukbb_eur/ldblk_ukbb_eur --bim_prefix=/data4/mchopra/GWAS_PGS_training/training_data/c_1_22 --sst_file=/data4/mchopra/GWAS_PGS_training/training_data/fastgwapgs/aao_input_prscs_se.txt --n_gwas=33372 --chrom=${SLURM_ARRAY_TASK_ID} --out_dir=/data4/mchopra/GWAS_PGS_training/training_data/PRScs_results/aao_${SLURM_ARRAY_TASK_ID} --write_pst=TRUE
